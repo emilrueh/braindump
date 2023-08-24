c3d87cf65713e241d9ccbea3e28d98117eec5df2
Emil Rühmland <emilrueh@googlemail.com>
2023-08-24 05:23:42 +0200

    Full rework including flags and opening of .txt

--- main.py
+++ main.py
@@ -25,9 +25,13 @@ from helpinghands.openai import call_whisper, call_gpt
 
 from helpinghands.utility.data import (
     insert_newlines,
-    append_to_or_create_txt_file,
+    write_to_txt_file,
+    load_text_from_file,
     open_txt_file,
 )
+
+from datetime import datetime
+import argparse
 
 
 def main():
@@ -53,18 +57,28 @@ def main():
     """
 
     try:
-        # ---directories---
+        # ---flags---
+        parser = argparse.ArgumentParser(
+            description=f"The flag -test allows for loading of previously recorded and transcribed files."
+        )
+        parser.add_argument(
+            "-test",
+            action="store_true",
+            help=f"This flag will set the project on runtime into a testing environment.",
+        )
+        args = parser.parse_args()
+        # ---flags---
 
+        # ---settings---
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+        # directories
         base_dir = os.path.dirname(__file__)
         settings_dir = os.path.join(base_dir, "settings")
         data_dir = os.path.join(base_dir, "data")
         audio_dir = os.path.join(data_dir, "audio")
         text_dir = os.path.join(data_dir, "text")
         backup_dir = os.path.join(data_dir, "backup")
-
-        # ---directories---
-        #
-        # ---settings---
 
         settings = load_settings(
             settings_file=os.path.join(settings_dir, "settings.json"),
@@ -73,46 +87,62 @@ def main():
 
         api_key = settings["OPENAI_API_KEY"]
         prompt = settings["PROMPT"]
-        audio_file_name = settings["AUDIO_FILE_NAME"]
 
-        recorder = AudioRecorder(
-            filename=audio_file_name, output_directory=audio_dir, duration=5
+        # file names
+        audio_file_name = f"{settings['AUDIO_FILE_NAME']}_{timestamp}"
+        transcript_file_name = f"{settings['TRANSCRIPT_FILE_NAME']}_{timestamp}"
+        summary_file_name = f"{settings['SUMMARY_FILE_NAME']}_{timestamp}"
+
+        test_transcript_file_name = os.path.join(
+            text_dir,
+            "backup",
+            f"{settings['TEST_TRANSCRIPT_FILE_NAME']}.txt",
         )
 
+        # audio recorder class initialization
+        recorder = AudioRecorder(
+            filename=audio_file_name,
+            output_directory=audio_dir,
+            duration=settings["RECORDING_LENGTH"],
+        )
         # ---settings---
-        #
-        # ---functions---
 
-        # recording (audio)
-        logger.info("Recording...")
-        recorded_file = recorder.record()
-        logger.info(f"Saved recording to: {recorded_file}")
-        output_file = recorded_file
-        logger.debug(f"output_file = {output_file}")  # DEBUG
+        if args.test:
+            transcript = load_text_from_file(test_transcript_file_name)
+            logger.info(f"Loading transcript from: {test_transcript_file_name}")
+        else:
+            # ---functions---
+            # recording (audio)
+            logger.info("Recording...")
+            output_audio_file = recorder.record()
+            logger.info(f"Saved recording to: {output_audio_file}")
 
-        # transcribing (whisper)
-        logger.info(f"Transcribing {output_file}")
-        transcript = call_whisper(api_key, output_file)
-        logger.debug(f"transcript = {transcript}")  # DEBUG
+            # transcribing (whisper)
+            logger.info(f"Transcribing {output_audio_file}")
+            transcript = call_whisper(api_key, output_audio_file)
+            logger.debug(f"transcript = {transcript}")  # DEBUG
+            write_to_txt_file(
+                transcript,
+                file_name=transcript_file_name,
+                output_directory=text_dir,
+                mode="write",
+            )
 
-        # analyzing (gpt)
-        logger.info("Analyzing...")
-        raw_summary = call_gpt(api_key, prompt=prompt, input_text=transcript)
-        logger.debug(f"raw_summary = {raw_summary}")  # DEBUG
+        if transcript:
+            # analyzing (gpt)
+            logger.info("Analyzing...")
+            raw_summary = call_gpt(api_key, prompt=prompt, input_text=transcript)
+            logger.debug(f"raw_summary = {raw_summary}")  # DEBUG
+            write_to_txt_file(
+                raw_summary,
+                file_name=summary_file_name,
+                output_directory=text_dir,
+                mode="write",
+            )
 
-        # printing (text)
-        fmt_summary = insert_newlines(raw_summary)
-        logger.debug(f"fmt_summary = {fmt_summary}")  # DEBUG
-
-        # v needs fixing of saving to .txt and opening in default text editor
-        # txt_file_path = "summary.txt"
-        # append_to_or_create_txt_file(summary, txt_file_path)
-        # summary = open_txt_file(txt_file_path)
-
-        summary = fmt_summary
-        print(summary)
-
-        # ---functions---
+            # presenting of output text via default .txt editor
+            open_txt_file(os.path.join(text_dir, f"{summary_file_name}.txt"))
+            # ---functions---
 
     except Exception as e:
         exception_name = log_exception(e, log_level="critical")
